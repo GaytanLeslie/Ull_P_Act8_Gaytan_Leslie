988bd0356c5df90bbb5faa63dfe9b75a6ec0b868
GaytanLeslie <91515331+GaytanLeslie@users.noreply.github.com>
2021-11-04 10:59:52 -0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+<footer>
+		<div class="wrapp">
+            <p> No lista 16 </p>
+            <p> // SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p><a href="https://github.com/GaytanLeslie/Ull_P_Act8_Gaytan_Leslie ">
+            <font color= "white"> https://github.com/GaytanLeslie/Ull_P_Act8_Gaytan_Leslie  </font> </a></p>
+		</div>
+	</footer>
