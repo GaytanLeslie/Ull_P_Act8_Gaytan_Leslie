9d3600c892abe2ac800a80481f8230f770472f6f
GaytanLeslie <91515331+GaytanLeslie@users.noreply.github.com>
2021-11-04 10:59:37 -0600

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-
-<footer>
-		<div class="wrapp">
-            <p> No lista 16 </p>
-            <p> // SUBMÓDULO 2
-                Desarrolla aplicaciones Web con conexión a bases de datos</p>
-            <p>https://github.com/GaytanLeslie/Ull_P_Act8_Gaytan_Leslie </p>
-		</div>
-	</footer>
